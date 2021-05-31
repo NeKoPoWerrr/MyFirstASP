1e78a30aac38a6c23ace44b50866a8732ce461d0
DESKTOP-40OA392\User <awp10520@gmail.com>
2021-05-31 19:36:59 +0800

    finished

---
+++ JokeWeb/Models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWeb.Models
+{
+    public class Jokes
+    {
+        public int ID { get; set; }
+        public  string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Jokes()
+        {
+
+        }
+    }
+   
+}
